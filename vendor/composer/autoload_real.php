05f89a498bce46178d955138ebe1e6a0208257a8
jinjianli <jinjianli21@163.com>
2021-01-13 21:48:46 +0900

    have mincrosoft trial

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,6 +22,8 @@ class ComposerAutoloaderInitd35d00583d0c17da1bda7ffb512c316b
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInitd35d00583d0c17da1bda7ffb512c316b', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInitd35d00583d0c17da1bda7ffb512c316b', 'loadClassLoader'));
@@ -32,7 +34,7 @@ class ComposerAutoloaderInitd35d00583d0c17da1bda7ffb512c316b
 
         $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
         if ($useStaticLoader) {
-            require_once __DIR__ . '/autoload_static.php';
+            require __DIR__ . '/autoload_static.php';
 
             call_user_func(\Composer\Autoload\ComposerStaticInitd35d00583d0c17da1bda7ffb512c316b::getInitializer($loader));
         } else {
